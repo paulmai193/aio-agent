738f0f0bef653233c3434d4cbe46293a3db48bf3
Dai Mai <dai.maithanh@gmail.com>
2025-09-07 16:13:23 +0700

    fix missing )

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -3,4 +3,4 @@ import sys
 import os
 
 # Add parent directory to path for imports
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
